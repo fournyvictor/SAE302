ab23c2d2e255581bde8cdaf5766f130184c00017
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-24 15:59:02 +0100

    mise au propre variables dans les strings et test css

--- js/app.js
+++ js/app.js
@@ -191,17 +191,17 @@ function createSeachDropdownHtmlList(array) {
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
                         <div class="col-md-4">
-                            <img src="https://image.tmdb.org/t/p/w154` + element.poster_path + `" class="img-fluid rounded-start" alt="` + element.title + `poster">
+                            <img src="https://image.tmdb.org/t/p/w154${element.poster_path}" class="img-fluid rounded-start" alt="${element.title} poster">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h5 class="card-title">`+ element.title + `</h5>
-                                    <p class="card-text"><small class="text-muted">`+ element.release_date + `</small></p>
-                                    <p class="card-text">`+ element.overview + `</p>
+                                <h5 class="card-title">${element.title}</h5>
+                                    <p class="card-text"><small class="text-muted">${element.release_date}</small></p>
+                                    <p class="card-text card-text-truncate">${element.overview}</p>
                             </div>
                         </div>
                     </div>
-                </div></a></li>`
+                </div></a></li>`;
         // html += '<li><img href="https://image.tmdb.org/t/p/w92' + element.poster_path + '"><a class="dropdown-item" href="#">' + element.title + '</a></li>';
     }
     console.debug(SEARCHBAR_DROPDOWN_LIST.innerHTML);
