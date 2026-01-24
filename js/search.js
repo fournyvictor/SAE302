4c05e42521b0d045725abc34213bf3ec60d8513f
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-24 22:39:21 +0100

    virer le style de merde dans la balise html

--- js/search.js
+++ js/search.js
@@ -62,7 +62,7 @@ function createSeachDropdownHtmlList(array) {
     let html = '';
     for (let element of array) {
         html += `<li><a href="https://webdev.fourny.org/victor/SAE302/film/?id=${element.id}">
-                <div class="card mb-3 dropdown-search-card" style="max-width: 800px;">
+                <div class="card mb-3 dropdown-search-card">
                     <div class="row g-0">
                         <div class="col-md-3">
                             <img src="https://image.tmdb.org/t/p/w154${element.poster_path}" class="img-fluid rounded-start" alt="${element.title} poster">
