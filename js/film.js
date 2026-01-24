2645b9c5021f7167adf9dbb3270784cb7b593d8a
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-24 16:52:50 +0100

    get details

--- js/film.js
+++ js/film.js
@@ -7,5 +7,22 @@ function main() {
 
     if (FILM_ID) {
         console.debug(FILM_ID);
+        console.debug(getFilmData(FILM_ID));
     }
+}
+
+async function getFilmData(ID) {
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzAwODk3NTQ0ZTUwZTg5N2Y4ZGZhNzlkNzY4YjcxNyIsIm5iZiI6MTY1NjI3ODM4NC4xNTYsInN1YiI6IjYyYjhjZDcwMTdjNDQzMDA2MDRiMjEwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZNZGW6-VnxqWAMDfqYZYtNRxbdZfLgqcMu3mysVMv-c'
+        }
+    };
+
+
+    const RESPONSE = await fetch(`https://api.themoviedb.org/3/movie/${ID}?language=en-US`, options);
+    const RESPONSE_JSON = await RESPONSE.json();
+    return RESPONSE_JSON;
 }
