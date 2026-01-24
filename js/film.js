31685b7bfba3af43a4b4cf3d175388f6106bf413
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-24 23:28:21 +0100

    implémentation indexedDB (quel enfer, 30min que je suis dessus c'est horrible)

--- js/film.js
+++ js/film.js
@@ -1,7 +1,7 @@
 const PARAMS = new URLSearchParams(document.location.search);
 const FILM_ID = PARAMS.get("id");
 const FILM_CONTAINER = document.getElementById("film-container");
-
+let is_movie_liked = false;
 
 
 main();
@@ -9,6 +9,7 @@ main();
 async function main() {
 
     if (FILM_ID) {
+        checkIfMovieLiked();
         console.debug(FILM_ID);
         const CAST = await getMovieCast(FILM_ID);
         console.debug(CAST[0]);
@@ -117,43 +118,102 @@ function makeFilmDisplayHtml(DATA, CAST) {
 
 }
 
+/******************************************************************************/
+/* INTERACTIONS DB                                                            */
+/******************************************************************************/
+
 function onLikeButtonClick() {
-    const dbName = "FullBoxdDB";
-    const request = indexedDB.open(dbName, 1);
+    const DB = "FullBoxdDB";
+    const REQUEST = indexedDB.open(DB, 1);
 
-    request.onerror = (event) => {
-        console.error("Erreur IndexedDB:", event.target.error);
-    };
+    REQUEST.onerror = onDBError;
+    if (is_movie_liked) {
+        REQUEST.onsuccess = onDBSuccessLikeRemove;
+    } else {
+        REQUEST.onsuccess = onDBSuccessLikeAdd;
 
-    // Création de la structure si elle n'existe pas (première ouverture ou changement de version)
-    request.onupgradeneeded = (event) => {
-        const db = event.target.result;
-        // On crée un store "likes" avec "filmId" comme clé unique
-        if (!db.objectStoreNames.contains("likes")) {
-            db.createObjectStore("likes", { keyPath: "filmId" });
-        }
-    };
+    }
 
-    request.onsuccess = (event) => {
-        const db = event.target.result;
-
-        // Transaction en lecture/écriture
-        const transaction = db.transaction(["likes"], "readwrite");
-        const objectStore = transaction.objectStore("likes");
-
-        // Ajout du film ID
-        const entry = { filmId: FILM_ID, addedAt: new Date() };
-        const addRequest = objectStore.add(entry);
-
-        addRequest.onsuccess = () => {
-            console.log(`Film ${FILM_ID} ajouté aux likes !`);
-        };
-
-        addRequest.onerror = (err) => {
-            console.error("Erreur ajout (déjà liké ?) :", err.target.error);
-        };
-    };
 }
 function onReviewButtonClick() {
     console.debug("AHAHAHA");
+}
+function checkIfMovieLiked() {
+    const DB = "FullBoxdDB";
+    const REQUEST = indexedDB.open(DB, 1);
+
+    REQUEST.onupgradeneeded = onDBUgradeNeeded;
+
+    REQUEST.onerror = onDBError;
+    REQUEST.onsuccess = onDBSuccessCheckLike;
+
+}
+
+function onDBError(event) {
+    console.error("Erreur IndexedDB:", event.target.error);
+}
+function onDBUgradeNeeded(event) {
+    const DB = event.target.result;
+    //creation de likes si non existant
+    if (!DB.objectStoreNames.contains("likes")) {
+        DB.createObjectStore("likes", { keyPath: "filmId" });
+    }
+    //creation de reviews si non existant
+    if (!DB.objectStoreNames.contains("reviews")) {
+        DB.createObjectStore("reviews", { keyPath: "filmId" });
+    }
+}
+function onDBSuccessLikeAdd(event) {
+    const DB = event.target.result;
+
+    const TRANSACTION = DB.transaction(["likes"], "readwrite");
+    const OBJECTSTORE = TRANSACTION.objectStore("likes");
+
+    // ajout du film aux likes
+    const ENTRY = { filmId: FILM_ID, addedAt: new Date() };
+    const REQUEST = OBJECTSTORE.add(ENTRY);
+
+    REQUEST.onsuccess = successfullyLiked;
+    REQUEST.onerror = dbTransactionError;
+}
+function onDBSuccessLikeRemove(event) {
+    const DB = event.target.result;
+
+    const TRANSACTION = DB.transaction(["likes"], "readwrite");
+    const OBJECTSTORE = TRANSACTION.objectStore("likes");
+
+    // retirer le film des likes
+    const REQUEST = OBJECTSTORE.delete(FILM_ID);
+
+    REQUEST.onsuccess = successfullyUnLiked;
+    REQUEST.onerror = dbTransactionError;
+}
+function onDBSuccessCheckLike(event) {
+    const DB = event.target.result;
+
+    const TRANSACTION = DB.transaction(["likes"], "readonly");
+    const OBJECTSTORE = TRANSACTION.objectStore("likes");
+
+    const REQUEST = OBJECTSTORE.get(FILM_ID);
+
+    REQUEST.onsuccess = onCheckLikedResult;
+    REQUEST.onerror = dbTransactionError;
+
+}
+function dbTransactionError(event) {
+    console.error("erreur de transaction db : ", event.target.error);
+}
+function successfullyLiked() {
+    console.log(`Film ${FILM_ID} ajouté aux likes !`);
+}
+function successfullyUnLiked() {
+    console.log(`Film ${FILM_ID} retiré des likes !`)
+}
+function onCheckLikedResult(event) {
+    const RESULT = event.target.result;
+
+    if (RESULT) {
+        console.log("Le film est liké");
+        is_movie_liked = true;
+    }
 }
