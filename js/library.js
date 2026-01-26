a20b49aa217dcce57460880aed8ce551957552f4
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-26 11:01:34 +0100

    zz

--- js/library.js
+++ js/library.js
@@ -27,7 +27,7 @@ async function createMovieListHtml() {
                     <h5 class="card-title fw-bold">${DATA.title}</h5>
                     <p class="card-text text-secondary mb-1">${YEAR}</p>
                     <p class="card-text d-none d-sm-block text-truncate">${DATA.overview}</p>
-                    <button class="btn btn-outline-light btn-sm mt-2" href="${element['filmId']}">Voir détails</button>
+                    <button class="btn btn-outline-light btn-sm mt-2" href="https://webdev.fourny.org/victor/SAE302/film/?id=${element['filmId']}">Voir détails</button>
                 </div>
             </div>
         </div>
