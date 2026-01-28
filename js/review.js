38c057f2c89d8198a5665b4ebd0c1925c847403f
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-28 18:25:05 +0100

    debug

--- js/review.js
+++ js/review.js
@@ -2,7 +2,7 @@ const PARAMS = new URLSearchParams(document.location.search);
 const FILM_ID = PARAMS.get("id");
 
 main();
-
+console.debug(FILM_ID);
 async function main() {
     REVIEW = getMovieReview(FILM_ID);
     console.debug(REVIEW);
