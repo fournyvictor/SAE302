0df6d7524b9c9c258385d49aa0cf0b5ded109bf6
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-28 23:13:14 +0100

    container tofay

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 
 
 const TRENDING_CONTAINER = document.getElementById("trending-container");
-
+const TRENDING_CONTAINER_TODAY = document.getElementById("trending-container-today");
 main();
 
 async function main() {
@@ -19,5 +19,17 @@ async function createTrendingCaroussel() {
     </a>`;
     }
     TRENDING_CONTAINER.innerHTML = html;
-
+}
+async function createTrendingCarousselDay() {
+    console.debug("createTrendingCarousselDay");
+    const MOVIES = await getTrendingMoviesToday();
+    console.debug(MOVIES);
+    let html = "";
+    for (element of MOVIES) {
+        console.debug("iter");
+        html += `<a href="./film/?id=${element.id}" class="trending-card">
+    <img src="https://image.tmdb.org/t/p/w342${element.poster_path}" class="img-fluid" alt="${element.title}">
+    </a>`;
+    }
+    TRENDING_CONTAINER_TODAY.innerHTML = html;
 }
