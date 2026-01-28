61fbba0a97e9348dc7c777eff7af586279805764
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-28 17:44:20 +0100

    test responsive

--- js/library.js
+++ js/library.js
@@ -28,10 +28,10 @@ async function createMovieListHtml() {
             review_text = REVIEW.review;
         } else { review_text = false; }
         if (!review_text) {
-            reviewHtml = `<p class="card-text d-none d-sm-block text-truncate">You did not write a review about ${DATA.title} yet. </p>
+            reviewHtml = `<p class="card-text card-text-truncate">You did not write a review about ${DATA.title} yet. </p>
                     <a class="write-review-button" href="https://webdev.fourny.org/victor/SAE302/review/?id=${DATA.id}"><button class="btn btn-outline-light btn-sm mt-2" >Write a review</button></a>`
         } else {
-            reviewHtml = `<p class="card-text d-none d-sm-block text-truncate">${review_text}</p>
+            reviewHtml = `<p class="card-text card-text-truncate">${review_text}</p>
                     <a class="write-review-button" href="https://webdev.fourny.org/victor/SAE302/review/?id=${DATA.id}"><button class="btn btn-outline-light btn-sm mt-2" >Edit your review</button></a>`
         }
         html += `<div class="card mb-3 w-100 border-0 bg-transparent text-white">
@@ -44,7 +44,7 @@ async function createMovieListHtml() {
                 <div class="card-body">
                     <h5 class="card-title fw-bold">${DATA.title}</h5>
                     <p class="card-text text-secondary mb-1">${YEAR}</p>
-                    <p class="card-text d-none d-sm-block card-text-truncate">${DATA.overview}</p>
+                    <p class="card-text card-text-truncate">${DATA.overview}</p>
                     <a href="https://webdev.fourny.org/victor/SAE302/film/?id=${DATA.id}">
                     <button class="btn btn-outline-light btn-sm mt-2" >See details</button></a>
                     <hr class="my-4">
