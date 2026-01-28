8fbb23083c8600b46d331e6eb434fd8fefa139fe
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-28 18:39:36 +0100

    submit system

--- js/review.js
+++ js/review.js
@@ -1,8 +1,23 @@
 const PARAMS = new URLSearchParams(document.location.search);
 const FILM_ID = PARAMS.get("id");
+const INPUT_FIELD = document.getElementById("input-field");
+const SUBMIT_BUTTON = document.getElementById("submit-button");
+
+SUBMIT_BUTTON.addEventListener("click", submitMovieReview.bind(FILM_ID, INPUT_FIELD.value));
 
 main();
 async function main() {
-    REVIEW = getMovieReview(FILM_ID);
+    REVIEW = await getMovieReview(FILM_ID);
     console.debug(REVIEW);
+    let review_text;
+    if (REVIEW) {
+        console.debug("review existante");
+        review_text = REVIEW.review;
+    } else {
+        console.debug("review inexistante");
+        review_text = null;
+    }
+    console.debug(review_text);
+
+
 }
