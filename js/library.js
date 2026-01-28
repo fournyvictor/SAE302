babb1bdd280d2d6d0abc6f930a60cd2c1bb06f78
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-28 17:10:48 +0100

    mise en forme carte library

--- js/library.js
+++ js/library.js
@@ -28,11 +28,10 @@ async function createMovieListHtml() {
                     <p class="card-text text-secondary mb-1">${YEAR}</p>
                     <p class="card-text d-none d-sm-block text-truncate">${DATA.overview}</p>
                     <a href="https://webdev.fourny.org/victor/SAE302/film/?id=${element['filmId']}">
-                    <button class="btn btn-outline-light btn-sm mt-2" >Voir détails</button></a>
+                    <button class="btn btn-outline-light btn-sm mt-2" >See details</button></a>
                     <hr class="my-4">
                     <h5 class="card-title fw-bold">Your review on ${DATA.title}</h5>
-
-                    <p class="card-text d-none d-sm-block text-truncate">You did not write a review yet. <a>Write a review</a></p>
+                    <p class="card-text d-none d-sm-block text-truncate">You did not write a review aubout ${DATA.title} yet. <a class="write-review-button" href="https://webdev.fourny.org/victor/SAE302/review/?id=${element.id}"><button class="btn btn-outline-light btn-sm mt-2" >Write a review</button></a></p>
                 </div>
             </div>
         </div>
