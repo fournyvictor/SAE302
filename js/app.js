1bfb32dac9f821ad5976e21800846d64af017ccf
VICTOR FOURNY <fournyvictor@gmail.com>
2026-01-29 01:23:08 +0100

    reloadmodal

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ const INSTALL_BUTTON = document.getElementById("install_button");
 const RELOAD_BUTTON = document.getElementById("reload_button");
 const IGNORE_BUTTON = document.getElementById("ignore-button");
 const INSTALL_MODAL = document.getElementById("install-modal");
+const RELOAD_MODAL = document.getElementById("reload-modal");
 const INSTALL_MODAL_OBJ = bootstrap.Modal.getOrCreateInstance(INSTALL_MODAL);
+const RELOAD_MODAL_OBJ = bootstrap.Modal.getOrCreateInstance(RELOAD_MODAL);
 const INSTALL_APP_BTN = document.getElementById("install-app-btn");
 const UPDATE_APP_BTN = document.getElementById("update-app-btn");
 
@@ -33,7 +35,7 @@ let beforeInstallPromptEvent;
 main();
 
 function main() {
-
+    RELOAD_MODAL_OBJ.hide();
     if (window.matchMedia("(display-mode: standalone)").matches) {
         console.log("Running as PWA");
         INSTALL_APP_BTN.classList.add("d-none");
@@ -138,6 +140,7 @@ function onStateChange(event) {
     if (SERVICE_WORKER.state == "installed" && navigator.serviceWorker.controller) {
         console.log("PWA Updated");
         RELOAD_BUTTON.disabled = false;
+        RELOAD_MODAL_OBJ.show();
         UPDATE_APP_BTN.classList.remove("d-none");
     }
 }
